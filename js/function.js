f6eaa67c3141bd7f74def6a3865addb192020b1c
Red-Spite <2529638156@qq.com>
2021-11-30 13:04:30 +0800

    first commit

--- js/function.js
+++ js/function.js
@@ -1,3 +1,16 @@
+
+
+/*
+
+    git 方法 
+    git clone https://github.com/Red-Spid/jsFunction.git
+    git add .
+    git commit -m "first commit"
+    git branch -M main
+    git push -u origin main
+*/
+
+
 /*
 
     js 原生方法
